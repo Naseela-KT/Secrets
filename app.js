62e26845828cf1b1d98be4827a1df52cdca95a86
Naseela-KT <75978405+Naseela-KT@users.noreply.github.com>
2023-01-28 11:53:34 +0530

    Level 1-Username and Password only

---
+++ app.js
@@ -0,0 +1,71 @@
+//jshint esversion:6
+
+const express=require("express");
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
+
+const app=express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1/userDB");
+
+const userSchema={
+    email:String,
+    password:String
+};
+
+// const secret="Thisisourlittlesecret";
+// userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+
+const User=new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/register",function(req,res){
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    })
+});
+
+app.post("/login",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser.password===password){
+                res.render("secrets");
+            }
+        }
+    })
+});
+
+
+
+app.listen(3000);
